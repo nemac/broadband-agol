5333675d06b92644ab8500e9a79a12154c1b84c1
Jeff Bliss <jeff.a.bliss@gmail.com>
2022-11-16 16:47:59 -0500

    more work in progress

--- handler.py
+++ handler.py
@@ -1,13 +1,12 @@
-#from arcgis import GIS
-#import json, os, pandas, requests
-import json
+from arcgis import GIS
+import json, os, pandas, requests
 
 # portal_url = "https://thisWillFail.maps.arcgis.com"
 # portal_user = "notAUser"
 # portal_password = "alsoNotAPassword"
 
-# gis = GIS(url=portal_url, username=portal_user, password=portal_password)
-# gis = GIS()
+#gis = GIS(url=portal_url, username=portal_user, password=portal_password)
+gis = GIS()
 
 def broadband(event, context):
     body = {
@@ -15,10 +14,10 @@ def broadband(event, context):
         "input": event,
     }
 
-    # try:
-        # print("ArcGIS Online as anonymous user")
-        # print("Logged in as anonymous user to " + gis.properties.portalName)
-    # except Exception as e:
-        # print(e)
+    try:
+        print("ArcGIS Online as anonymous user")
+        print("Logged in as anonymous user to " + gis.properties.portalName)
+    except Exception as e:
+        print(e)
 
     return {"statusCode": 200, "body": json.dumps(body)}
