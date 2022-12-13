c48ee1066bec4586561aa8815bb32a2ec5f6b1cf
seanebum <smatthe2@unca.edu>
2022-12-13 16:23:13 -0500

    now uses passed argument instead of hard coded

--- generateData.py
+++ generateData.py
@@ -13,14 +13,14 @@ def generate_data(input_data):
     ookla_data = geopandas.read_file(os.path.join(sample_dir, 'ookola_fixed.gpkg'))
 
     # Currently only creates fields for ookla fixed dataset
-    poly_of_interest = sample_input.geometry[0]
+    poly_of_interest = input_data.geometry[0]
     targ_rows = ookla_data[ookla_data['geometry'].map(lambda shape: shape.intersects(poly_of_interest))] # YAY, This gets ALL The Data for a queried intersection!
-    new_cols = list(set(targ_rows.columns.tolist()) - set(sample_input.columns.tolist())) # These are the columns that are in the output but not the input
+    new_cols = list(set(targ_rows.columns.tolist()) - set(input_data.columns.tolist())) # These are the columns that are in the output but not the input
     form_cols = ['ookla_fixed_'+new_col for new_col in new_cols]
 
     # print(form_cols)
-    output_df = sample_input.reindex(columns = sample_input.columns.tolist() + new_cols)
-    for index, geom in enumerate(sample_input.geometry): # Iterates all sample shapes
+    output_df = input_data.reindex(columns = input_data.columns.tolist() + new_cols)
+    for index, geom in enumerate(input_data.geometry): # Iterates all sample shapes
         raw_data = ookla_data[ookla_data['geometry'].map(lambda shape: shape.intersects(geom))]
     for i, col_name in enumerate(new_cols):
         output_df.loc[index, form_cols[i]] = raw_data[col_name].mean()
