956dc8a086924882a6cbe1a93f548447bcb9fe8b
Jeff Bliss <jeff.a.bliss@gmail.com>
2023-01-10 10:29:48 -0500

    small helper script that currently prints out last pushed data row

---
+++ recoverData.py
@@ -0,0 +1,19 @@
+from arcgis import GIS
+from arcgis.features import Feature
+from copy import deepcopy
+
+agol_username = 'jbliss-nemac'
+agol_password = '8kwMB97A1eOJ'
+gis = GIS("https://unca.maps.arcgis.com/home/index.html", agol_username, agol_password)
+
+test_output = "d3454682e9844327be5d18fd76ef9d6b"
+output_to_recover = "9cfbc22bfadb4be7b5127f50714c16a8"
+output_content = gis.content.get(output_to_recover)
+test_content = gis.content.get(test_output)
+test_content_layer = test_content.layers[0]
+test_content_fset = test_content_layer.query()
+test_content_features = test_content_fset.features
+print(test_content_features[-1])
+#id_field = dict(deepcopy(test_content.layers[0].properties.fields[1]))
+
+#print(output_content.layers[0].manager)
