f347b3fdb6f2e80f5a842a8f4dd060bd58f865f6
Jeff Bliss <jeff.a.bliss@gmail.com>
2023-01-25 15:44:01 -0500

    basic framework for the custom functions

---
+++ customFunctionsLogic.py
@@ -0,0 +1,53 @@
+"""This file will only exist until we migrate it to the actual real custom function file
+We can delete this when done.
+Every function in here is a special use case for the fields that require calculations
+from previous calculations
+"""
+
+####### FUNC1 Functions
+def calculate_fccnew_summary_speedtier(data):
+    return 'dummy_value_fccnew_summary_speedtier'
+
+def calculate_fccnew_speedtier(data):
+    return 'dummy_value_fccnew_speedtier'
+
+def calculate_address_persqmeter(data):
+    return 0
+
+def calculate_percent_addresses(data):
+    return 987654321
+
+def calculate_adress_rank(data):
+    return 123456789
+
+def calculate_fccnew_techquestionable(data):
+    return 987654321
+
+def calculate_fccnew_need_more_ook(data):
+    return 123456789
+
+def calculate_fccnew_need_survey(data):
+    return 987654321
+
+def calculate_fccnew_speed_questionable(data):
+    return 123456789
+
+def calculate_fccold_techquestionable(data):
+    return 987654321
+
+def calculate_fccold_need_more_ook(data):
+    return 123456789
+
+def calculate_fccold_need_survey(data):
+    return 987654321
+
+def calculate_fccold_speed_questionable(data):
+    return 123456789
+
+
+###### FUNC2 Functions
+def calculate_fccnew_questionable(data):
+    return 987654321
+
+def calculate_fccold_questionable(data):
+    return 123456789
